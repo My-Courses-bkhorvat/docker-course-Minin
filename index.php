afc02c199dc3cf595b514a974f6b097d5a41ff5f
bkhorvat <bkhorvat.php@gmail.com>
2022-12-03 15:09:34 +0100

    Study base comads

--- index.php
+++ index.php
@@ -20,6 +20,13 @@ echo 'Hello world!';
  * docker rm 2a144d29cef5
  * docker rm bc5a47f7f163 62629b00c620
  * docker container prune
- *
+ * docker run -d -p 80:3000
+ * docker attach
+ * docker logs
+ * docker run -d -p 3000:3000 --name --rm
+ * docker rmi
+ * docker image prune -a
+ * docker build -t logsapp .
+ * docker build -t logsapp:exc .
  *
  */
