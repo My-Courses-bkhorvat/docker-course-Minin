d573cd7d9dfe5e3469dbebed4cd2ffab267e8546
bkhorvat <bkhorvat.php@gmail.com>
2022-12-03 13:22:28 +0100

    use docker comands, add logs-app

--- index.php
+++ index.php
@@ -6,4 +6,20 @@ echo 'Hello world!';
  * docker build .
  * docker image ls
  * docker run 2a144d29cef5
+ *
+ * docker hub node
+ *
+ * docker pull node
+ *
+ * docker images
+ * docker run node
+ * docker ps
+ * docker ps --help
+ * docker ps -a
+ * docker run -it node
+ * docker rm 2a144d29cef5
+ * docker rm bc5a47f7f163 62629b00c620
+ * docker container prune
+ *
+ *
  */
